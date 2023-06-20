ef21b879bfd3b5b15c7230cded67e19511aecd52
Rhuszzz <helouqi1nav@naver.com>
2023-06-20 14:48:01 +0900

    controller

---
+++ final project/src/main/java/multi/com/finalproject/minihome/controller/MiniHomeRestController.java
@@ -0,0 +1,18 @@
+package multi.com.finalproject.minihome.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class MiniHomeRestController {
+	
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		log.info("");
+//		
+//		return "";
+//	}
+	
+}
