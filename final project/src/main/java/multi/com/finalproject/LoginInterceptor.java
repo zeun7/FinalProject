703c5ae74fddfc72492e29e24e26003a660aa6e2
z._.n <kzeun425@gmail.com>
2023-06-21 17:37:49 +0900

    delete posthandler

--- final project/src/main/java/multi/com/finalproject/LoginInterceptor.java
+++ final project/src/main/java/multi/com/finalproject/LoginInterceptor.java
@@ -41,10 +41,4 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		return true;
 	}
 	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		log.info("postHandle()....");
-	}
-	
 }
