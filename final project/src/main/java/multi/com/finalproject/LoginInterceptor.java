8c736a351740fecdcbaa044c97e83ed28405b5fc
z._.n <kzeun425@gmail.com>
2023-06-22 10:42:50 +0900

    LoginInterceptor

--- final project/src/main/java/multi/com/finalproject/LoginInterceptor.java
+++ final project/src/main/java/multi/com/finalproject/LoginInterceptor.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import lombok.extern.slf4j.Slf4j;
