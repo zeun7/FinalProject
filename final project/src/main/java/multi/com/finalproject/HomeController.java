0a1066734f1bb0990658a6bc628f6452cc15349d
z._.n <kzeun425@gmail.com>
2023-07-13 21:54:55 +0900

    merge board 0713

--- final project/src/main/java/multi/com/finalproject/HomeController.java
+++ final project/src/main/java/multi/com/finalproject/HomeController.java
@@ -19,57 +19,65 @@ import multi.com.finalproject.miniboard.model.MiniBoardVO;
 @Slf4j
 @Controller
 public class HomeController {
-
+	
 	@Autowired
 	HomeService service;
-
-	@RequestMapping(value = { "/", "/home.do" }, method = RequestMethod.GET)
+	
+	@RequestMapping(value = {"/", "/home.do"}, method = RequestMethod.GET)
 	public String home() {
 		log.info("/home.do...");
-
+		
 		return "home";
 	}
-
-	@ResponseBody
-	@RequestMapping(value = "/json_post_friends.do", method = RequestMethod.GET)
-	public List<MiniBoardVO> json_post_friends(MemberVO vo, int limit, String sortKey) {
-		log.info("/json_post_friends.do...");
-		log.info("vo:{}", vo);
-		log.info("limit:{}", limit);
-		log.info("sortKey:{}", sortKey);
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", vo.getId());
-		map.put("limit", limit);
-		map.put("sortKey", sortKey);
-
-		List<MiniBoardVO> vos = service.friends_post(map);
-
-		return vos;
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/json_post_board.do", method = RequestMethod.GET)
-	public List<BoardVO> json_post_board(BoardVO vo, int limit, String sortKey) {
-		log.info("/json_post_board.do...");
-		log.info("vo:{}", vo);
-		log.info("limit:{}", limit);
-		log.info("sortKey:{}", sortKey);
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("bname", vo.getBname());
-		map.put("limit", limit);
-		map.put("sortKey", sortKey);
-
-		List<BoardVO> vos = service.board_post(map);
-
-		return vos;
-	}
-
+	
 	@RequestMapping(value = "/template.do", method = RequestMethod.GET)
 	public String template() {
 		log.info("/template.do...");
 		
 		return "template";
 	}
+	
+	@RequestMapping(value = "/dashboard.do", method = RequestMethod.GET)
+	public String dashboard() {
+		log.info("/dashboard.do...");
+		
+		return "dashboard";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/json_post_friends.do",  method = RequestMethod.GET)
+	public List<MiniBoardVO> json_post_friends(MemberVO vo, int limit, String sortKey) {
+		log.info("/json_post_friends.do...");
+		log.info("vo:{}", vo);
+		log.info("limit:{}", limit);
+		log.info("sortKey:{}", sortKey);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", vo.getId());
+		map.put("limit", limit);
+		map.put("sortKey", sortKey);
+		
+		List<MiniBoardVO> vos = service.friends_post(map);
+		
+		return vos;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/json_post_board.do",  method = RequestMethod.GET)
+	public List<BoardVO> json_post_board(BoardVO vo, int limit, String sortKey) {
+		log.info("/json_post_board.do...");
+		log.info("vo:{}", vo);
+		log.info("limit:{}", limit);
+		log.info("sortKey:{}", sortKey);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("bname", vo.getBname());
+		map.put("limit", limit);
+		map.put("sortKey", sortKey);
+		
+		List<BoardVO> vos = service.board_post(map);
+		
+		return vos;
+	}
+
 }
